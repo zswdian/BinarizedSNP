891fa72ca5a0fd35e0550d759e40ef5b7f79916b
dianzsw <3260274746@qq.com>
2021-06-21 16:14:55 +0800

    some changes

--- Models/BinarizedModules.py
+++ Models/BinarizedModules.py
@@ -2,32 +2,6 @@ import torch
 import torch.nn as nn
 
 
-class BinActive(torch.autograd.Function):
-
-    def forward(self, input):
-        self.save_for_backward(input)
-        n = input[0].nelement()
-        s = input.size()
-        beta = input.norm(1, 3, keepdim=True)\
-            .sum(2, keepdim=True).sum(1, keepdim=True).div(n)
-        return input.sign().mul(beta.expand_as(s))
-
-    def backward(self, grad_output):
-        input, = self.saved_tensors
-        n = input[0].nelement()
-        s = input.size()
-        grad_input = input.norm(1, 3, keepdim=True).sum(2, keepdim=True)\
-            .sum(1, keepdim=True).div(n).expand(s)
-        grad_input[input.lt(-1.0)] = 0
-        grad_input[input.gt(1.0)] = 0
-        grad_input = grad_input.mul(grad_output)
-        grad_input_add = input.sign().mul(grad_output)
-        grad_input_add = grad_input_add.sum(3, keepdim=True).sum(2, keepdim=True)\
-            .sum(1, keepdim=True).div(n).expand(s)
-        grad_input_add = grad_input_add.mul(input.sign())
-        return grad_input.add(grad_input_add).mul(1.0 - 1.0 / s[1]).mul(n)
-
-
 class BinConv2d(nn.Module):
 
     def __init__(self, input_channels, output_channels, kernel_size=-1,
@@ -50,7 +24,6 @@ class BinConv2d(nn.Module):
     def forward(self, input):
         x = self.bn(input)
         # x = self.pRelu(input)
-        x = BinActive()(x)
         if self.dropout_ratio != 0:
             x = self.dropout(x)
         x = self.conv(x)
