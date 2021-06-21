4135adfbd126fe5e72921b4c74691a78dfcafd66
dianzsw <3260274746@qq.com>
2021-06-21 15:40:58 +0800

    some changes

--- Models/BinarizedModules.py
+++ Models/BinarizedModules.py
@@ -6,16 +6,26 @@ class BinActive(torch.autograd.Function):
 
     def forward(self, input):
         self.save_for_backward(input)
-        mean = torch.mean(input.abs(), 1, keepdim=True)
-        input = input.sign()
-        return input, mean
+        n = input[0].nelement()
+        s = input.size()
+        beta = input.norm(1, 3, keepdim=True)\
+            .sum(2, keepdim=True).sum(1, keepdim=True).div(n)
+        return input.sign().mul(beta.expand_as(s))
 
-    def backward(self, grad_output, grad_output_mean):
+    def backward(self, grad_output):
         input, = self.saved_tensors
-        grad_input = grad_output.clone()
-        grad_input[input.le(-1)] = 0
-        grad_input[input.ge(1)] = 0
-        return grad_input
+        n = input[0].nelement()
+        s = input.size()
+        grad_input = input.norm(1, 3, keepdim=True).sum(2, keepdim=True)\
+            .sum(1, keepdim=True).div(n).expand(s)
+        grad_input[input.lt(-1.0)] = 0
+        grad_input[input.gt(1.0)] = 0
+        grad_input = grad_input.mul(grad_output)
+        grad_input_add = input.sign().mul(grad_output)
+        grad_input_add = grad_input_add.sum(3, keepdim=True).sum(2, keepdim=True)\
+            .sum(1, keepdim=True).div(n).expand(s)
+        grad_input_add = grad_input_add.mul(input.sign())
+        return grad_input.add(grad_input_add).mul(1.0 - 1.0 / s[1]).mul(n)
 
 
 class BinConv2d(nn.Module):
@@ -28,23 +38,21 @@ class BinConv2d(nn.Module):
         self.stride = stride
         self.padding = padding
         self.dropout_ratio = dropout
-        # self.bn = nn.BatchNorm2d(input_channels, eps=1e-4, momentum=0.1, affine=True)
-        # self.bn.weight.data = self.bn.weight.data.zero_().add(1.0)
+        self.bn = nn.BatchNorm2d(input_channels, eps=1e-4, momentum=0.1, affine=True)
+        self.bn.weight.data = self.bn.weight.data.zero_().add(1.0)
         if dropout != 0:
             self.dropout = nn.Dropout(dropout)
         self.conv = nn.Conv2d(input_channels, output_channels, kernel_size=kernel_size,
                               stride=stride, padding=padding)
-        self.avg = nn.AvgPool2d(kernel_size=kernel_size, stride=stride, padding=padding)
-        self.pRelu = nn.PReLU(0.25)
+        # self.pRelu = nn.PReLU(0.25)
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, input):
-        # x = self.bn(input)
-        x = self.pRelu(input)
-        x, mean = BinActive()(x)
+        x = self.bn(input)
+        # x = self.pRelu(input)
+        x = BinActive()(x)
         if self.dropout_ratio != 0:
             x = self.dropout(x)
         x = self.conv(x)
-        beta = self.avg(mean)
-        x = x.mul(beta)
-        # x = self.relu(x)
+        x = self.relu(x)
         return x
--- Models/net_binary.py
+++ Models/net_binary.py
@@ -7,28 +7,9 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.xnor = nn.Sequential(
-            # nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
-            # nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
-            # nn.ReLU(inplace=True),
-            # BinConv2d(192, 160, kernel_size=1, stride=1, padding=0),
-            # BinConv2d(160, 96, kernel_size=1, stride=1, padding=0),
-            # nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-            #
-            # BinConv2d(96, 192, kernel_size=5, stride=1, padding=2, dropout=0.5),
-            # BinConv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            # BinConv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            # nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
-            #
-            # BinConv2d(192, 192, kernel_size=3, stride=1, padding=1, dropout=0.5),
-            # BinConv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            # nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
-            # nn.Conv2d(192, 10, kernel_size=1, stride=1, padding=0),
-            # nn.ReLU(inplace=True),
-            # nn.AvgPool2d(kernel_size=8, stride=1, padding=0),
-
-            nn.ReLU(inplace=True),
             nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
-            # nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
+            nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
+            nn.ReLU(inplace=True),
             BinConv2d(192, 160, kernel_size=1, stride=1, padding=0),
             BinConv2d(160, 96, kernel_size=1, stride=1, padding=0),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
@@ -40,11 +21,30 @@ class Net(nn.Module):
 
             BinConv2d(192, 192, kernel_size=3, stride=1, padding=1, dropout=0.5),
             BinConv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            # nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
-            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
             nn.Conv2d(192, 10, kernel_size=1, stride=1, padding=0),
-
+            nn.ReLU(inplace=True),
             nn.AvgPool2d(kernel_size=8, stride=1, padding=0),
+
+            # nn.ReLU(inplace=True),
+            # nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
+            # # nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
+            # BinConv2d(192, 160, kernel_size=1, stride=1, padding=0),
+            # BinConv2d(160, 96, kernel_size=1, stride=1, padding=0),
+            # nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            #
+            # BinConv2d(96, 192, kernel_size=5, stride=1, padding=2, dropout=0.5),
+            # BinConv2d(192, 192, kernel_size=1, stride=1, padding=0),
+            # BinConv2d(192, 192, kernel_size=1, stride=1, padding=0),
+            # nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
+            #
+            # BinConv2d(192, 192, kernel_size=3, stride=1, padding=1, dropout=0.5),
+            # BinConv2d(192, 192, kernel_size=1, stride=1, padding=0),
+            # # nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
+            # nn.ReLU(inplace=True),
+            # nn.Conv2d(192, 10, kernel_size=1, stride=1, padding=0),
+            #
+            # nn.AvgPool2d(kernel_size=8, stride=1, padding=0),
         )
 
     def forward(self, x):
