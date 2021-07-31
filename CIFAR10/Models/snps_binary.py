b4bc8ff93c35095e7cef738cec4ddfc019fb38a9
dianzsw <3260274746@qq.com>
2021-07-31 22:14:57 +0800

    add imagenet

--- CIFAR10/Models/snps_binary.py
+++ CIFAR10/Models/snps_binary.py
@@ -8,8 +8,8 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.snps = nn.Sequential(
             nn.PReLU(),
+            nn.BatchNorm2d(3, eps=1e-4, momentum=0.1, affine=False),
             nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
             BinSNPSConv2d(192, 160, kernel_size=1, stride=1, padding=0),
             BinSNPSConv2d(160, 96, kernel_size=1, stride=1, padding=0),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
