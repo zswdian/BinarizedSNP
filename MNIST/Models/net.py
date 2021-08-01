b271b81c44caeabbca020bb4db20f031dc63d5dc
dianzsw <3260274746@qq.com>
2021-08-02 03:10:57 +0800

    add imagenet

--- MNIST/Models/net.py
+++ MNIST/Models/net.py
@@ -6,7 +6,7 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        self.net = nn.Sequential(
+        self.features = nn.Sequential(
             nn.Conv2d(1, 20, kernel_size=5, stride=1),
             nn.BatchNorm2d(20, eps=1e-4, momentum=0.1, affine=False),
             nn.PReLU(),
@@ -19,13 +19,13 @@ class Net(nn.Module):
         )
 
         self.classifier = nn.Sequential(
-            nn.Conv2d(50 * 4 * 4, 500, kernel_size=4, stride=1, padding=0),
+            nn.Linear(50 * 4 * 4, 500),
             nn.PReLU(),
             nn.Linear(500, 10),
         )
 
     def forward(self, x):
-        x = self.net(x)
+        x = self.features(x)
         x = x.view(x.size(0), 50 * 4 * 4)
         x = self.classifier(x)
         return x
