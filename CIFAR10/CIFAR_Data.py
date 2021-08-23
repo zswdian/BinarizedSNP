daaa3a75c13dded3909f4971182f2007d4cd371f
dianzsw <3260274746@qq.com>
2021-08-24 00:30:58 +0800

    update lr adjust

--- CIFAR10/CIFAR_Data.py
+++ CIFAR10/CIFAR_Data.py
@@ -2,19 +2,27 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.utils.data
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
 
-DOWNLOAD = True
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+DOWNLOAD = False
 
 trainset = torchvision.datasets.CIFAR10(root='Data', train=True,
-                                        download=DOWNLOAD, transform=transform)
+                                        download=DOWNLOAD, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
                                           shuffle=True, num_workers=8, pin_memory=True)
 
 testset = torchvision.datasets.CIFAR10(root='Data', train=False,
-                                       download=DOWNLOAD, transform=transform)
+                                       download=DOWNLOAD, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100,
                                          shuffle=False, num_workers=8, pin_memory=True)
 
--- CIFAR10/main.py
+++ CIFAR10/main.py
@@ -101,12 +101,12 @@ def test(expt_no):
     return
 
 
-def adjust_learning_rate(optimizer, epoch):
-    update_list = [120, 160, 180]
-    if epoch in update_list:
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = param_group['lr'] * 0.1
-    return
+# def adjust_learning_rate(optimizer, epoch):
+#     update_list = [120, 160, 180]
+#     if epoch in update_list:
+#         for param_group in optimizer.param_groups:
+#             param_group['lr'] = param_group['lr'] * 0.1
+#     return
 
 
 if __name__ == '__main__':
@@ -214,8 +214,10 @@ if __name__ == '__main__':
         model = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
 
         # define solver and criterion
-        optimizer = optim.Adam(model.parameters(), lr=0.01, weight_decay=0.00001)
         criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                              momentum=0.9, weight_decay=5e-4)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
         # define the binarization operator
         if not args.full:
@@ -229,9 +231,10 @@ if __name__ == '__main__':
         best_acc = 0
 
         for epoch in range(1, epochs + 1):
-            adjust_learning_rate(optimizer, epoch)
+            # adjust_learning_rate(optimizer, epoch)
             train(epoch, i + 1)
             test(i + 1)
+            scheduler.step()
 
         with open(filename, 'a') as f:
             f.write('Expt {}: Best Accuracy: {:.2f}%\n'.format(i + 1, best_acc))
