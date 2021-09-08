94562a789b985bc1f9fde8935e9ba28916b72c80
dianzsw <3260274746@qq.com>
2021-09-08 20:32:24 +0800

    update nin_snp.py

--- CIFAR10/Models/nin_snp.py
+++ CIFAR10/Models/nin_snp.py
@@ -5,45 +5,54 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.snps = nn.Sequential(
-            nn.PReLU(),
-            nn.BatchNorm2d(3, eps=1e-4, momentum=0.1, affine=False),
+        self.classifier = nn.Sequential(
+            nn.ReLU(inplace=True),
             nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
-            nn.PReLU(),
-            nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
+            nn.BatchNorm2d(192),
+
+            nn.ReLU(inplace=True),
             nn.Conv2d(192, 160, kernel_size=1, stride=1, padding=0),
-            nn.PReLU(),
-            nn.BatchNorm2d(160, eps=1e-4, momentum=0.1, affine=True),
+            nn.BatchNorm2d(160),
+
+            nn.ReLU(inplace=True),
             nn.Conv2d(160, 96, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(96),
+
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            nn.Dropout2d(0.5),
 
-            nn.PReLU(),
-            nn.BatchNorm2d(96, eps=1e-4, momentum=0.1, affine=True),
             nn.Conv2d(96, 192, kernel_size=5, stride=1, padding=2),
-            nn.Dropout2d(0.5),
-            nn.PReLU(),
-            nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=True),
-            nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.PReLU(),
-            nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=True),
-            nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.PReLU(),
-            nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(192),
 
-            nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=True),
-            nn.Conv2d(192, 192, kernel_size=3, stride=1, padding=1),
-            nn.Dropout2d(0.5),
-            nn.PReLU(),
-            nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.PReLU(),
-            nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
+            nn.BatchNorm2d(192),
+
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(192),
+
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
+            nn.Dropout2d(0.5),
+
+            nn.Conv2d(192, 192, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(192),
+
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(192),
+
+            nn.ReLU(inplace=True),
             nn.Conv2d(192, 10, kernel_size=1, stride=1, padding=0),
-            nn.PReLU(),
+            nn.BatchNorm2d(10),
+
+            nn.ReLU(inplace=True),
             nn.AvgPool2d(kernel_size=8, stride=1, padding=0),
         )
 
     def forward(self, x):
-        x = self.snps(x)
+        x = self.classifier(x)
         x = x.view(x.size(0), 10)
         return x
