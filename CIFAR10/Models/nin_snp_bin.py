2f7416838b2fc6b6600932f8334cb09c5d978417
dianzsw <3260274746@qq.com>
2021-09-08 20:32:38 +0800

    update nin_snp_bin.py

--- CIFAR10/Models/nin_snp_bin.py
+++ CIFAR10/Models/nin_snp_bin.py
@@ -6,10 +6,10 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.snps = nn.Sequential(
-            nn.PReLU(),
-            nn.BatchNorm2d(3, eps=1e-4, momentum=0.1, affine=False),
+        self.classifier = nn.Sequential(
+            nn.ReLU(inplace=True),
             nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
             BinSNPConv2d(192, 160, kernel_size=1, stride=1, padding=0),
             BinSNPConv2d(160, 96, kernel_size=1, stride=1, padding=0),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
@@ -21,14 +21,13 @@ class Net(nn.Module):
 
             BinSNPConv2d(192, 192, kernel_size=3, stride=1, padding=1, dropout=0.5),
             BinSNPConv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.PReLU(),
             nn.BatchNorm2d(192, eps=1e-4, momentum=0.1, affine=False),
             nn.Conv2d(192, 10, kernel_size=1, stride=1, padding=0),
-            nn.PReLU(),
+            nn.ReLU(inplace=True),
             nn.AvgPool2d(kernel_size=8, stride=1, padding=0),
         )
 
     def forward(self, x):
-        x = self.snps(x)
+        x = self.classifier(x)
         x = x.view(x.size(0), 10)
         return x
