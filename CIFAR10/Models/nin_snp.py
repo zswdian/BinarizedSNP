bd5bbb89769cf287a5ae6cc1cbe917cf694f6c1f
dianzsw <3260274746@qq.com>
2021-09-08 21:31:04 +0800

    替换bn和conv的顺序

--- CIFAR10/Models/nin_snp.py
+++ CIFAR10/Models/nin_snp.py
@@ -7,47 +7,45 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.classifier = nn.Sequential(
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(3),
             nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
 
-            nn.ReLU(inplace=True),
             nn.BatchNorm2d(192),
+            nn.ReLU(inplace=True),
             nn.Conv2d(192, 160, kernel_size=1, stride=1, padding=0),
 
-            nn.ReLU(inplace=True),
             nn.BatchNorm2d(160),
+            nn.ReLU(inplace=True),
             nn.Conv2d(160, 96, kernel_size=1, stride=1, padding=0),
 
+            nn.BatchNorm2d(96),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
             nn.Dropout2d(0.5),
-
             nn.Conv2d(96, 192, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(192),
 
+            nn.BatchNorm2d(192),
             nn.ReLU(inplace=True),
             nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(192),
 
+            nn.BatchNorm2d(192),
             nn.ReLU(inplace=True),
             nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(192),
 
+            nn.BatchNorm2d(192),
             nn.ReLU(inplace=True),
             nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
             nn.Dropout2d(0.5),
-
             nn.Conv2d(192, 192, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(192),
 
+            nn.BatchNorm2d(192),
             nn.ReLU(inplace=True),
             nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(192),
 
+            nn.BatchNorm2d(192),
             nn.ReLU(inplace=True),
             nn.Conv2d(192, 10, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(10),
 
+            nn.BatchNorm2d(10),
             nn.ReLU(inplace=True),
             nn.AvgPool2d(kernel_size=8, stride=1, padding=0),
         )
