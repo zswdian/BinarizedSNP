4d18a3c0c06dcf9373ea27bc6e211cdc90ff9eb4
dianzsw <3260274746@qq.com>
2021-09-27 15:45:35 +0800

    修改alexnet

--- ImageNet/Models/alexnet.py
+++ ImageNet/Models/alexnet.py
@@ -1,6 +1,35 @@
 import torch.nn as nn
 import torch
 
+
+class LRN(nn.Module):
+    def __init__(self, local_size=1, alpha=1.0, beta=0.75, ACROSS_CHANNELS=True):
+        super(LRN, self).__init__()
+        self.ACROSS_CHANNELS = ACROSS_CHANNELS
+        if ACROSS_CHANNELS:
+            self.average=nn.AvgPool3d(kernel_size=(local_size, 1, 1),
+                    stride=1,
+                    padding=(int((local_size-1.0)/2), 0, 0))
+        else:
+            self.average=nn.AvgPool2d(kernel_size=local_size,
+                    stride=1,
+                    padding=int((local_size-1.0)/2))
+        self.alpha = alpha
+        self.beta = beta
+
+    def forward(self, x):
+        if self.ACROSS_CHANNELS:
+            div = x.pow(2).unsqueeze(1)
+            div = self.average(div).squeeze(1)
+            div = div.mul(self.alpha).add(1.0).pow(self.beta)
+        else:
+            div = x.pow(2)
+            div = self.average(div)
+            div = div.mul(self.alpha).add(1.0).pow(self.beta)
+        x = x.div(div)
+        return x
+
+
 class Net(nn.Module):
 
     def __init__(self):
@@ -9,34 +38,41 @@ class Net(nn.Module):
         self.features = nn.Sequential(
             nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
             nn.BatchNorm2d(96, eps=1e-4, momentum=0.1, affine=True),
-            nn.PReLU(),
+            nn.ReLU(inplace=True),
+            LRN(local_size=5, alpha=0.0001, beta=0.75),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.BatchNorm2d(96, eps=1e-4, momentum=0.1, affine=True),
+
             nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2, groups=1),
-            nn.PReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
             nn.BatchNorm2d(256, eps=1e-4, momentum=0.1, affine=True),
+            nn.ReLU(inplace=True),
+            LRN(local_size=5, alpha=0.0001, beta=0.75),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+
             nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
-            nn.PReLU(),
             nn.BatchNorm2d(384, eps=1e-4, momentum=0.1, affine=True),
+            nn.ReLU(inplace=True),
+
             nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1, groups=1),
-            nn.PReLU(),
             nn.BatchNorm2d(384, eps=1e-4, momentum=0.1, affine=True),
+            nn.ReLU(inplace=True),
+
             nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1, groups=1),
-            nn.PReLU(),
+            nn.BatchNorm2d(256, eps=1e-4, momentum=0.1, affine=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
 
         self.classifier = nn.Sequential(
-            nn.BatchNorm1d(256 * 6 * 6, eps=1e-4, momentum=0.1, affine=True),
             nn.Linear(256 * 6 * 6, 4096),
-            nn.PReLU(),
             nn.BatchNorm1d(4096, eps=1e-4, momentum=0.1, affine=True),
-            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
             nn.Dropout(0.5),
-            nn.PReLU(),
-            nn.BatchNorm1d(4096, eps=1e-3, momentum=0.1, affine=True),
-            nn.Dropout(),
+
+            nn.Linear(4096, 4096),
+            nn.BatchNorm1d(4096, eps=1e-4, momentum=0.1, affine=True),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+
             nn.Linear(4096, 1000),
         )
 
@@ -45,6 +81,7 @@ class Net(nn.Module):
         x = x.view(x.size(0), 256 * 6 * 6)
         x = self.classifier(x)
         return x
+
 
 def net(pretrained=False, **kwargs):
 
