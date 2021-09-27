6b16cb88bd335e37279b73ea5a7fb262d25ee6d3
dianzsw <3260274746@qq.com>
2021-09-27 22:23:56 +0800

    修改nin-snp

--- CIFAR10/Models/nin_snp.py
+++ CIFAR10/Models/nin_snp.py
@@ -6,48 +6,46 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.classifier = nn.Sequential(
-            nn.PReLU(),
             nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
+
             nn.BatchNorm2d(192),
-
-            nn.PReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(192, 160, kernel_size=1, stride=1, padding=0),
+
             nn.BatchNorm2d(160),
-
-            nn.PReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(160, 96, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(96),
-
-            nn.PReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
             nn.Dropout2d(0.5),
 
+            nn.BatchNorm2d(96),
+            nn.ReLU(inplace=True),
             nn.Conv2d(96, 192, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(192),
 
-            nn.PReLU(),
+            nn.BatchNorm2d(192),
+            nn.ReLU(inplace=True),
             nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(192),
 
-            nn.PReLU(),
+            nn.BatchNorm2d(192),
+            nn.ReLU(inplace=True),
             nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(192),
-
-            nn.PReLU(),
             nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
             nn.Dropout2d(0.5),
 
+            nn.BatchNorm2d(192),
+            nn.ReLU(inplace=True),
             nn.Conv2d(192, 192, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(192),
 
-            nn.PReLU(),
+            nn.BatchNorm2d(192),
+            nn.ReLU(inplace=True),
             nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(192),
 
-            nn.PReLU(),
+            nn.BatchNorm2d(192),
+            nn.ReLU(inplace=True),
             nn.Conv2d(192, 10, kernel_size=1, stride=1, padding=0),
+
             nn.BatchNorm2d(10),
-            nn.PReLU(),
+            nn.ReLU(inplace=True),
             nn.AvgPool2d(kernel_size=8, stride=1, padding=0),
         )
 
