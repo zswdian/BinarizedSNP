72a89cf9a40ac8d51d3d6ba897c97089ff7d79b1
dianzsw <3260274746@qq.com>
2021-09-27 14:29:40 +0800

    修改resnet-snp结构，bn放在activ前面

--- CIFAR10/Models/resnet_snp.py
+++ CIFAR10/Models/resnet_snp.py
@@ -8,13 +8,13 @@ class BasicBlock(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.bn2 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.prelu = nn.PReLU()
+        self.relu = nn.ReLU(inplace=True)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
@@ -25,8 +25,8 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = self.conv1(self.bn1(self.prelu(x)))
-        out = self.conv2(self.bn2(self.prelu(out)))
+        out = self.conv1(self.relu(self.bn1(x)))
+        out = self.conv2(self.relu(self.bn2(out)))
         out += self.shortcut(x)
         return out
 
@@ -36,15 +36,15 @@ class Bottleneck(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1):
         super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+        self.bn3 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, self.expansion *
                                planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes)
-        self.prelu = nn.PReLU()
+        self.relu = nn.ReLU(inplace=True)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
@@ -55,9 +55,9 @@ class Bottleneck(nn.Module):
             )
 
     def forward(self, x):
-        out = self.conv1(self.bn1(self.prelu(x)))
-        out = self.conv2(self.bn2(self.prelu(out)))
-        out = self.conv3(self.bn3(self.prelu(out)))
+        out = self.conv1(self.relu(self.bn1(x)))
+        out = self.conv2(self.relu(self.bn2(out)))
+        out = self.conv3(self.relu(self.bn3(out)))
         out += self.shortcut(x)
         return out
 
@@ -74,8 +74,9 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.bn1 = nn.BatchNorm2d(512)
+        self.relu = nn.ReLU(inplace=True)
         self.linear = nn.Linear(512*block.expansion, num_classes)
-        self.prelu = nn.PReLU()
+
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -86,13 +87,13 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = self.conv1(self.prelu(x))
+        out = self.conv1(x)
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
         out = self.bn1(out)
-        out = self.prelu(out)
+        out = self.relu(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
