02fd4b349f7503f07fa80187c6c0fe1c30f7ebbb
dianzsw <3260274746@qq.com>
2021-09-28 20:12:32 +0800

    修改vgg-snp

--- CIFAR10/Models/vgg_snp.py
+++ CIFAR10/Models/vgg_snp.py
@@ -4,25 +4,23 @@ import torch.nn as nn
 
 
 cfg = {
-    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512],
-    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512],
-    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512],
-    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512],
+    'VGG11': ['M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
 }
 
 
 class VGG(nn.Module):
     def __init__(self, vgg_name):
         super(VGG, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
         self.features = self._make_layers(cfg[vgg_name])
-        self.prelu = nn.PReLU()
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.classifier = nn.Linear(512, 10)
 
     def forward(self, x):
-        out = self.features(x)
-        out = self.prelu(out)
-        out = self.maxpool(out)
+        out = self.conv1(x)
+        out = self.features(out)
         out = out.view(out.size(0), -1)
         out = self.classifier(out)
         return out
