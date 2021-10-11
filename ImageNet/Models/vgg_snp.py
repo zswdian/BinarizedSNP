6e223d1b265f9aece6955bde56de492a94b8feb1
dianzsw <3260274746@qq.com>
2021-10-11 14:37:04 +0800

    修改vgg-snp

--- ImageNet/Models/vgg_snp.py
+++ ImageNet/Models/vgg_snp.py
@@ -4,23 +4,21 @@ import torch.nn as nn
 
 
 cfg = {
-    'VGG11': ['M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    'VGG13': [64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    'VGG16': [64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
-    'VGG19': [64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
 }
 
 
 class VGG(nn.Module):
     def __init__(self, vgg_name):
         super(VGG, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
         self.features = self._make_layers(cfg[vgg_name])
         self.classifier = nn.Linear(512, 10)
 
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.features(out)
+        out = self.features(x)
         out = out.view(out.size(0), -1)
         out = self.classifier(out)
         return out
