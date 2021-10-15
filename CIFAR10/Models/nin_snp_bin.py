4718c8e8b1ed0823708f327da2b444ddbd38c65c
dianzsw <3260274746@qq.com>
2021-10-15 14:30:57 +0800

    修改nin-snp-bin

--- CIFAR10/Models/nin_snp_bin.py
+++ CIFAR10/Models/nin_snp_bin.py
@@ -7,6 +7,8 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.classifier = nn.Sequential(
+            nn.BatchNorm2d(3),
+            nn.ReLU(inplace=True),
             nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
             BinSNPConv2d(192, 160, kernel_size=1, stride=1, padding=0),
             BinSNPConv2d(160, 96, kernel_size=1, stride=1, padding=0),
